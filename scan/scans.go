f4c44876c6ff8f560de1f2da19dfb1a05eec6bea
Epictetus <36168644+Epictetus24@users.noreply.github.com>
2020-07-18 20:54:35 +0100

    Add files via upload

--- scan/scans.go
+++ scan/scans.go
@@ -1,7 +1,6 @@
 package scan
 
 import (
-	"fmt"
 	"os/exec"
 	"sync"
 
@@ -70,11 +69,10 @@ func Gobust(host Host, wg *sync.WaitGroup) {
 
 	defer wg.Done()
 
-	args := []string{"dir", "-u", "hostname", "-w", "/opt/SecLists/Discovery/Web-Content/raft-small-words-lowercase.txt", "-o", "hostname-gobuster"}
+	args := []string{"dir", "-u", "hostname", "-w", "/opt/SecLists/Discovery/Web-Content/raft-small-words-lowercase.txt", "-o", "hostname-gobuster", "-t4"}
 	args[2] = host.Hostname
 	filename := host.Hostname + "-gobust.txt"
 	args[6] = filename
-	fmt.Println(args)
 
 	gobust := exec.Command("/usr/bin/gobuster", args[0:]...)
 	if err := gobust.Start(); err != nil {
@@ -82,16 +80,18 @@ func Gobust(host Host, wg *sync.WaitGroup) {
 		return
 	}
 
-	color.Blue("gobust running against host %s\n", host.Hostname)
+	color.Yellow("gobust running against host %s\n", host.Hostname)
 
 	if err := gobust.Wait(); err != nil {
 		color.Red("gobust returned error: %v", err)
 	}
 
-	color.Green("gobust finished, file for %s saved as %s.\n", host.Hostname, args[7])
+	color.Green("gobust finished, file for %s saved as %s.\n", host.Hostname, args[6])
 }
 
-func Whatweb(host Host) {
+func Whatweb(host Host, wg *sync.WaitGroup) {
+
+	defer wg.Done()
 
 	args := []string{"-v", "-a", "4", "host", "--log-verbose="}
 	args[3] = host.Hostname
@@ -113,10 +113,11 @@ func Whatweb(host Host) {
 	color.Green("gobust finished, file for %s saved as %s.\n", host.Hostname, args[7])
 }
 
-func Nmap(host Host) {
+func Nmap(host Host, wg *sync.WaitGroup) {
+	defer wg.Done()
 
-	args := []string{"-vv", "-n", "-sV", "-Pn", "-O", "-oA", "full_tcp", "-p0-", "IP_ADDRESS"}
-	args[8] = host.IP
+	args := []string{"-sV", "-O", "-Pn", "host.co.uk", "-p0-", "-oA", "full_tcp", "-T3", "-vv"}
+	args[3] = host.Hostname
 	filename := "fulltcp_nmap_" + host.Hostname
 	args[6] = filename
 
@@ -126,11 +127,11 @@ func Nmap(host Host) {
 		return
 	}
 
-	color.Blue("nmap full tcp running against host %s\n", host.Hostname)
+	color.Green("nmap full tcp running against host %s\n", host.Hostname)
 
 	if err := gobust.Wait(); err != nil {
 		color.Red("nmap returned error: %v", err)
 	}
 
-	color.Green("nmap finished, file for %s saved as %s.\n", host.Hostname, args[7])
+	color.Green("nmap finished, file for %s saved as %s.\n", host.Hostname, args[6])
 }
